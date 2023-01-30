de4248a9c43fc90ca52bc31e12c5b8ff66b28548
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-01-30 18:45:02 +0000

    Started working on the main menu.

---
+++ Assets/Scripts/Canvas/MainMenu/mM.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mM : MonoBehaviour
+{
+    [SerializeField] private Canvas mMCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenSettings()
+    {
+        
+    }
+}
